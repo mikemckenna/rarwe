8154bd70b71c13e29aeb077ce80e828e9256584a
Mike McKenna <junkmckenn@gmail.com>
2016-07-10 08:11:02 -0400

    Completed Chapter 7: Components

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -18,5 +18,11 @@ export default Ember.Route.extend({
       band.get('songs').pushObject(song);
       controller.set('title', '');
     }
+
+    // updateRating: function(params) {
+    //   var song = params.item;
+    //   var rating = params.rating;
+    //   song.set('rating', rating);
+    // }
   }
 });

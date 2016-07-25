f452f20ddf473448be5bc3a4c134d3314885e26b
Mike McKenna <junkmckenn@gmail.com>
2016-07-25 09:42:05 -0400

    completed Chapter 12: Sorting and searching with query parameters

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's',
+  },
   songCreationStarted: false,
+  sortBy: 'ratingDesc',
+  searchTerm: '',
+
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
 
   canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
     return this.get('songCreationStarted') || this.get('model.songs.length');
